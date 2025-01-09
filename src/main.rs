4b953ad9a982d101783241045fbfe1e2c3411690
Erick Christian <erickchristian48@gmail.com>
2025-01-09 11:30:10 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::{
+    env::{self, Args},
+    fs,
+};
+
+fn main() {
+    let args = parse_args(env::args());
+    let config = parse_config(&args);
+
+Config::new(args)
+
+    let file_content = fs::read_to_string(config.file_path).expect("File cannot be read!");
+
+    println!("file content: {file_content}");
+}
+
+fn parse_args(args: Args) -> Vec<String> {
+    args.skip(1).collect()
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args.first().expect("No query provided!").to_owned();
+
+        let file_path = args.get(1).expect("No file path provided!").to_string();
+
+        Config { query, file_path }
+    }
+}
